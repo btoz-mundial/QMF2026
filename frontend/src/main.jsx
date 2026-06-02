e5eb2cd3aea430ca57edd3bfc10dec97eedfef43
Beto Z <josue.zapata@zeconsultores.com>
2026-06-02 15:58:34 -0600

    Added Microsoft Clarity

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import '@/styles/globals.css'
 import App from './App'
 
+// Microsoft Clarity
+;(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}
+    t=l.createElement(r)
+    t.async=1
+    t.src="https://www.clarity.ms/tag/"+i
+    y=l.getElementsByTagName(r)[0]
+    y.parentNode.insertBefore(t,y)
+})(window, document, "clarity", "script", "x0xq52c00u")
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
